e99e85371f4766504ce88872fca07ed2492a834b
Federico <as>
2021-04-09 11:34:56 +0100

    clean up and reformat notes

--- data/small_tasks/perc_under_z.py
+++ data/small_tasks/perc_under_z.py
@@ -12,21 +12,21 @@ p_all = 0
 
 # scan all files and append the useful points to the list
 for root, dirs, files in os.walk(path):
-	for name in files:
-		if name.startswith('Filtered_'):
-			# create dictionary from csv
-			points_file = os.path.join(root, name)
-			csv_file = open(points_file)
-			csv_dict = csv.DictReader(csv_file, delimiter=';')
-			
-			# count Z points under -0.02
-			for pos in csv_dict:
-				z_pos = float(pos['Z'])
-				if z_pos <= z:
-					p_under += 1
-				
-				# count point
-				p_all += 1
+    for name in files:
+        if name.startswith('Filtered_'):
+            # create dictionary from csv
+            points_file = os.path.join(root, name)
+            csv_file = open(points_file)
+            csv_dict = csv.DictReader(csv_file, delimiter=';')
+            
+            # count Z points under -0.02
+            for pos in csv_dict:
+                z_pos = float(pos['Z'])
+                if z_pos <= z:
+                    p_under += 1
+                
+                # count point
+                p_all += 1
 
 # print results
 print(f"""
